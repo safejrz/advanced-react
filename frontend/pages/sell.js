1117bcad1d007bce4188341fe5b9c1c745b7984a
Javier Rivera <jzavala@accuray.com>
2020-12-28 16:48:06 -0600

    Creating a home page
    Creating an index.js file, and git.ignore file.
    
    03 - An Intro to Next.js, Tooling and Routing

---
+++ frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+const Sell = (props) => (
+  <div>
+    <p>Sell</p>
+    <Link href="/">Home!</Link>
+  </div>
+);
+
+export default Sell;
