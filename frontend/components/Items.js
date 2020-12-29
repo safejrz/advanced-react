6c62554d92a2f91fc8a307367eabe939650d4781
Javier Rivera <jzavala@accuray.com>
2020-12-28 20:15:01 -0600

    Commented code that caused displaying error
    Commented code that caused displaying error

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -8,7 +8,11 @@ import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
-    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
+    items(first: $first, skip: $skip, ) { 
+      # orderBy: createdAt_DESC ▲
+      # commented code beacuse of the error
+      # expected value of type 'ItemOrderByInput' but got: "createdAt_DESC"
+            
       id
       title
       price
