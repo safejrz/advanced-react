775c7cd89a463180d84675793b7773042f1bd556
Javier Rivera <jzavala@accuray.com>
2021-01-01 19:09:52 -0600

    Deleting msg log as potential security issue

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -111,7 +111,6 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken, resetTokenExpiry },
     });
-    console.log(res);
     return { message: 'Thanks!' };
     // 3. Email them that reset token
   },
