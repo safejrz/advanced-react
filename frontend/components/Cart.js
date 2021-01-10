8344fc258d0364e3e73d2b338b3f600f8f786255
Javier Rivera <jzavala@accuray.com>
2021-01-10 00:10:56 -0600

    48 - Search Items

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Query, Mutation } from "react-apollo";
-import gql from "graphql-tag";
-import { adopt } from "react-adopt";
-import User from "./User";
-import CartStyles from "./styles/CartStyles";
-import Supreme from "./styles/Supreme";
-import CloseButton from "./styles/CloseButton";
-import SickButton from "./styles/SickButton";
-import CartItem from "./CartItem";
-import calcTotalPrice from "../lib/calcTotalPrice";
-import formatMoney from "../lib/formatMoney";
+import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
+import User from './User';
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
+import CartItem from './CartItem';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import formatMoney from '../lib/formatMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -25,9 +25,7 @@ const TOGGLE_CART_MUTATION = gql`
 /* eslint-disable */
 const Composed = adopt({
   user: ({ render }) => <User>{render}</User>,
-  toggleCart: ({ render }) => (
-    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
-  ),
+  toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
   localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
 });
 /* eslint-enable */
@@ -45,7 +43,7 @@ const Cart = () => (
             </CloseButton>
             <Supreme>{me.name}'s Cart</Supreme>
             <p>
-              You Have {me.cart.length} Item{me.cart.length === 1 ? "" : "s"} in your cart.
+              You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.
             </p>
           </header>
           <ul>
